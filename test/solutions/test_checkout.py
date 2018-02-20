40c8b5a949a8ca9d7adb7a9928ba3810f55fbbd9
sbx_user1059 <sbx_user1059@ip-10-12-71-111.eu-west-2.compute.internal>
2018-02-20 21:26:20 +0000

    Tue Feb 20 21:26:20 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -17,5 +17,14 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(checkout('AAABBBCDEE'), 290)
         self.assertEqual(checkout('AAABBCDEE'), 275)
 
+    def test_A_new_discount(self):
+        self.assertEqual(checkout('AAAA'), 180)
+        self.assertEqual(checkout('AAAAA'), 200)
+        self.assertEqual(checkout('AAAAAA'), 250)
+        self.assertEqual(checkout('AAAAAAA'), 275)
+        self.assertEqual(checkout('AAAAAAAA'), 290)
+        self.assertEqual(checkout('AAAAAAAAA'), 275)
+        self.assertEqual(checkout('AAAAAAAAA'), 275)
+
 if __name__ == '__main__':
     unittest.main()
