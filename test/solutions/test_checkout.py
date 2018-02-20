760fb62332972342ead2dccaf1ba4f2a2614c809
sbx_user1059 <sbx_user1059@ip-10-12-71-111.eu-west-2.compute.internal>
2018-02-20 21:08:51 +0000

    Tue Feb 20 21:08:51 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -7,9 +7,11 @@ class TestCheckout(unittest.TestCase):
     def test_checkout(self):
             self.assertEqual(checkout('AAABBCD'), 210)
 
-            self.assertEqual(checkout('AAAABBC'), 210)
+            self.assertEqual(checkout('AAAABBC'), 245)
 
+            self.assertEqual(checkout('E'), -1)
 
+            self.assertEqual(checkout('CCC'), 60)
 
 if __name__ == '__main__':
     unittest.main()
